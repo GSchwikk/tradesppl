66dc915cf2ede3d736f3d3cde8346dbde1e4f8ad
GSchwikk <gschwikk@gmail.com>
2016-02-05 19:18:21 +0200

    Added category views and adjusted profile views for categories

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,14 @@ class Profile < ActiveRecord::Base
     belongs_to :user
     has_and_belongs_to_many :categories
 
+    validate :validate_categories
+
     has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
     
+  def validate_categories
+    errors.add(:categories, "too many categories, select 3 maximum") if categories.size > 3
+  end
+
+
 end
