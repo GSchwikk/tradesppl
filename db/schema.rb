80c92da724cbad338cd3e9efad06061362ba93bf
GSchwikk <gschwikk@gmail.com>
2016-02-05 19:34:44 +0200

    Created user admins, updated category controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205164952) do
+ActiveRecord::Schema.define(version: 20160205172031) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -59,12 +59,12 @@ ActiveRecord::Schema.define(version: 20160205164952) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20160205164952) do
     t.datetime "updated_at"
     t.integer  "role_id"
     t.string   "stripe_customer_token",  limit: 255
+    t.boolean  "admin",                              default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
